b484997bca2a627413c64474e8f910c42262b7e7
Ricardo Luis Mender <rlmender@gmail.com>
2013-12-09 16:51:55 -0300

    Agrego lo insert para los roles del admin (roles que van por defecto y son necesarios por el mod. Auth).

--- usuarios.sql
+++ usuarios.sql
@@ -7,3 +7,10 @@ INSERT INTO `roles` (`id`, `name`, `description`) VALUES
 # Agregamos el usuario 'admin' y la contraseña 'password' (sin comillas)
 INSERT INTO `users` (`username`, `email`, `password`) VALUES
 ('admin', 'admin@example.com','3aa4b899cda42cb4079f07ad4f84dec0f790f67735c61cab192fd7507b6f05ee');
+
+# Agrego unos roles por defecto a el usuario admin.
+# En principio el user_id debe ser 1 ya que el primer usuario que debería
+# ser creado es el admin. En caso de que no sea así debe ser el user_id
+# correspondiente al admin.
+INSERT INTO `fedeh`.`roles_users` (`user_id`,`role_id`)
+VALUES (1,1), (1,3);
