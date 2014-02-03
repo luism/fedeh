2a856aabeb5a6503dc5d09cab9abd96f53de3c42
Cristian Lafuente <cristian_ht@hotmail.com>
2014-02-03 20:39:06 -0300

    Cambio en Persona y modelo rol creado

--- application/classes/Model/Persona.php
+++ application/classes/Model/Persona.php
@@ -1,10 +1,12 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
- 
+
+// EL nombre derl modelo debe conicidir con la tabla pero en singular 
 class Model_Persona extends ORM {
  
     public function rules()
     {
         return array(
+            // Aca van los atributos que coniciden con los de la tabla EXACTO y siempre en minuculas
             'nombre' => array(
                 array('not_empty'),
                 // array('min_length', array(':value', 4)),
