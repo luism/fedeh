8e98ffd55b9c55b364b1461ebda52ba35346f3d0
Ricardo Luis Mender <rlmender@gmail.com>
2014-04-02 20:12:03 -0300

    Agrego un script de sql para agregar los tipos de lineas de cuentas corrientes

---
+++ tipos_lineas_cuenta_corriente.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT * FROM fedeh.tipos_cuentas_corrientes
+LIMIT 0, 1000
+
+-- Date: 2014-04-02 20:11
+*/
+INSERT INTO `tipos_cuentas_corrientes` (`id`,`tipocuenta`) VALUES (1,'socio');
+INSERT INTO `tipos_cuentas_corrientes` (`id`,`tipocuenta`) VALUES (2,'judicial');
+INSERT INTO `tipos_cuentas_corrientes` (`id`,`tipocuenta`) VALUES (3,'donacion');
+INSERT INTO `tipos_cuentas_corrientes` (`id`,`tipocuenta`) VALUES (4,'otro plan');
