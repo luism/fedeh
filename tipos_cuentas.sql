fcdc2011ee582ba2e0c89c2320b248ad1650b10b
Ricardo Luis Mender <rlmender@gmail.com>
2014-04-02 17:24:45 -0300

    Agrego los tipo de cuenta a la base de datos.

---
+++ tipos_cuentas.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: SELECT * FROM fedeh.tipos_plan_cuentas
+LIMIT 0, 1000
+
+-- Date: 2014-04-02 17:08
+*/
+INSERT INTO `tipos_plan_cuentas` (`id`,`TipoPlan`) VALUES (1,'efectivo debito');
+INSERT INTO `tipos_plan_cuentas` (`id`,`TipoPlan`) VALUES (2,'efectivo credito');
+INSERT INTO `tipos_plan_cuentas` (`id`,`TipoPlan`) VALUES (3,'cheque debito');
+INSERT INTO `tipos_plan_cuentas` (`id`,`TipoPlan`) VALUES (4,'cheque credito');
+INSERT INTO `tipos_plan_cuentas` (`id`,`TipoPlan`) VALUES (5,'donacion efectivo');
+INSERT INTO `tipos_plan_cuentas` (`id`,`TipoPlan`) VALUES (11,'donacion cheque');
