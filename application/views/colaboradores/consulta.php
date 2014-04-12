c0918de3c09cd85cdcd2a08fbb38b1c17c900311
cristian_hl <cristian_hl@hotmail.com>
2014-04-11 22:21:18 -0300

    Agrego pequeñas modificaciones a las vistas de Consultar

--- application/views/colaboradores/consulta.php
+++ application/views/colaboradores/consulta.php
@@ -6,20 +6,13 @@
         <div class="panel-heading">
         Apellido
           <?php echo Form::input('apellido', $apellido, array('class' => 'input-medium search-query', 'placeholder' => '', 'autofocus')) ?>
-          <!--<?php //echo Form::input('last_name', S_POST['last_name'], array('class' => 'form-control', 'placeholder' => '', 'autofocus')) ?>-->
-          <!--<button type="submit" class="btn btn-primary">Consultar</button>-->
-        </div>
-        <div class="panel-heading">
+        
         Nombre
           <?php echo Form::input('name', $name, array('class' => 'input-medium search-query', 'placeholder' => '', 'autofocus')) ?>
-          <!--<?php //echo Form::submit('save', 'Consultar', array('name' => 'save', 'class' => 'btn btn-primary')); ?>-->
-        </div>
-        <div class="panel-heading">
+         
         Documento
           <?php echo Form::input('doc', $doc, array('class' => 'input-medium search-query', 'placeholder' => '', 'autofocus')) ?>
-          <!--<?php //echo Form::submit('save', 'Consultar', array('name' => 'save', 'class' => 'btn btn-primary')); ?>-->
-        </div>
-        <div class="panel-heading">
+         
           <?php echo Form::submit('save', 'Consultar', array('last_name' => 'save', 'class' => 'btn btn-primary')); ?>
         </div>
       <?php echo Form::close(); ?><!-- Fin de Formulario -->
