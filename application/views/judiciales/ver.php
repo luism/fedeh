d5a20782f592d96b5552a1b87ce928ef623d7b46
cristian_hl <cristian_hl@hotmail.com>
2014-04-20 21:20:35 -0300

    Agrego archivo Ver para ver la cuenta generada de judiciales

---
+++ application/views/judiciales/ver.php
@@ -0,0 +1,26 @@
+<h3>Nombre y Apellido: <?php echo $persona->nombre ?> <?php echo $persona->apellido ?>
+</h3>
+
+
+<p>
+  Se genero la Cuenta:
+  <?php $judicial->persona->tiene_cuenta() ? 'si' : 'no <a href="#">Generar Cuenta</a>' ?>
+</p>
+<p>Plan de cuenta: <?php echo $plan_de_cuenta->id ?></p>
+
+<table>
+  <tr>
+    <th>Cuota</th>
+    <th>Monto</th>
+    <th>Detalle</th>
+    <th>Fecha Vto</th>
+  </tr>
+<?php foreach ($lineas_cuentas_corrientes as $i => $linea){ ?>
+  <tr>
+    <td><?php echo $i +1 ?></td>
+    <td><?php echo $linea->debe ?></td>
+    <td><?php echo $linea->detalle ?></td>
+    <td><?php echo Helper_Date::format($linea->fecha_cta_cte) ?></td>
+  </tr>
+<?php } ?>
+</table>
