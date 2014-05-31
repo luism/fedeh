478cc98159013bc2fbd95dcf38c1d5300d836646
Ricardo Luis Mender <rlmender@gmail.com>
2014-05-31 11:26:21 -0300

    Agrego un template para la pagina de inicio. Ademas agrego paneles para ir poniendo los graficos con datos.

--- application/classes/Controller/Dashboard.php
+++ application/classes/Controller/Dashboard.php
@@ -5,5 +5,6 @@ class Controller_Dashboard extends Controller_Template_Base
 
   public function action_index()
   {
+    $this->template->content = View::factory('dashboard/index');
   }
 }
---
+++ application/views/dashboard/index.php
@@ -0,0 +1,62 @@
+<div class="row">
+    <div class="col-md-4">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">Panel title</h3>
+          </div>
+          <div class="panel-body">
+            Panel content
+          </div>
+          <div class="panel-footer">Panel footer</div>
+        </div>        
+    </div>
+    <div class="col-md-4">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">Panel title</h3>
+          </div>
+          <div class="panel-body">
+            Panel content
+          </div>
+          <div class="panel-footer">Panel footer</div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="panel panel-default">
+          <div class="panel-heading"><h3 class="panel-title">Panel title</h3></div>
+          <div class="panel-body">Panel content</div>
+          <div class="panel-footer">Panel footer</div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-4">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">Panel title</h3>
+          </div>
+          <div class="panel-body">
+            Panel content
+          </div>
+          <div class="panel-footer">Panel footer</div>
+        </div>        
+    </div>
+    <div class="col-md-4">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">Panel title</h3>
+          </div>
+          <div class="panel-body">
+            Panel content
+          </div>
+          <div class="panel-footer">Panel footer</div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="panel panel-default">
+          <div class="panel-heading"><h3 class="panel-title">Panel title</h3></div>
+          <div class="panel-body">Panel content</div>
+          <div class="panel-footer">Panel footer</div>
+        </div>
+    </div>
+</div>
